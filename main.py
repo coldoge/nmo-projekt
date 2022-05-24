387be8a3ba4873cc1ef287a733265af6f96f9810
janscheuring <jans@meritus.pl>
2022-05-24 16:29:41 +0200

    the display of the results has been completely changed

--- main.py
+++ main.py
@@ -1,55 +1,37 @@
-import math
-import random
-import numpy as np
-from modules import Population
+import pygame
+from locations_generator import *
+from config import *
+from visualization import draw_text, draw_dots, draw_lines
 
-number_of_locations = 5
+# pygame initialization
+pygame.init()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((width, height))
+pygame.font.init()
+my_font = pygame.font.SysFont(font, font_size)
 
+# generation of the first generation in the genetic algorithm
+locations, locations_numbers = generate_locations_list(number_of_locations - 1, minmaxCoordinates)[0],\
+                               generate_locations_list(number_of_locations - 1, minmaxCoordinates)[1]
+actual_generation = generate_population(solutions_in_generation, locations_numbers,
+                                        np.array(get_distance_array(locations)))
 
-def generate_locations_list(_number_of_locations):
-    _locations = []
-    _locations_numbers = list(range(0,_number_of_locations+1))
-    for _ in range(_number_of_locations + 1):
-        _locations.append([random.randint(1, 10) for _ in range(2)])
-    return _locations, _locations_numbers
-
-
-def get_distance_array(_locations):
-    _distance_array = []
-    for location1 in _locations:
-        row = []
-        for location2 in _locations:
-            row.append(math.dist(location1, location2))
-        _distance_array.append(row)
-    return _distance_array
-
-
-#TODO change way of creating an array
-def generate_first_generation(_population_size, _locations_numbers):
-    first = np.asarray([np.random.permutation(_locations_numbers) for _ in range(_population_size)])
-    generation = []
-    for route in first:
-        route1 = list(route)
-        index_1 = route1.index(0)
-        route1[0], route1[index_1] = 0, route1[0]
-        route1.append(0)
-        generation.append(route1)
-    return generation
-
-
-#TODO change way of creating Population
-def generate_population(_population_size, _locations_numbers, _distance_array):
-    return Population(generate_first_generation(_population_size, _locations_numbers), _distance_array)
-
-
-locations, locations_numbers = generate_locations_list(number_of_locations - 1)[0], generate_locations_list(number_of_locations - 1)[1]
-
-generacja = generate_population(10, locations_numbers, np.array(get_distance_array(locations)))
-
-print(locations)
-print(generacja.generation)
-print(generacja.distance_array)
-generacja.calculate_lengths_of_routes()
-generacja.get_best_results()
-print(generacja.lengths_list)
-print(generacja.best_route_distance, generacja.best_route)
+# visualization and generating generation loop
+while state:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            state = False
+    screen.fill(white)
+    actual_generation.calculate_lengths_of_routes()
+    actual_generation.get_best_results()
+    actual_generation.get_roulette_wheel()
+    route = actual_generation.get_route(locations)
+    draw_dots(route, screen)
+    draw_lines(route, screen)
+    draw_text(my_font, screen, actual_generation)
+    actual_generation.update_population()
+    actual_generation.mutation(mutation_rate)
+    pygame.display.update()
+    clock.tick(60)
+pygame.quit()
+quit()
