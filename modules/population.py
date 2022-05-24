ca73436aa3ee3f3cf69fc4adc83f6aaa86939b09
janscheuring <jans@meritus.pl>
2022-05-24 16:27:38 +0200

    added genetic algorithm operators and more attributes of the Population class

--- modules/population.py
+++ modules/population.py
@@ -1,13 +1,22 @@
 import math
-class Population():
+import random
+from itertools import chain
+
+
+class Population:
     def __init__(self, generation, distance_array):
         self.generation = generation
         self.distance_array = distance_array
         self.lengths_list = []
         self.best_route_distance = math.inf
         self.best_route = None
+        self.roulette_wheel = None
+        self.best_result_list = []
+        self.best_route_locations = None
+        self.number_of_generation = 1
 
     def calculate_lengths_of_routes(self):
+        self.lengths_list = []
         for route in self.generation:
             distance = 0
             for i in range(len(route)-1):
@@ -18,7 +27,77 @@ class Population():
         if self.best_route_distance > min(self.lengths_list):
             self.best_route_distance = min(self.lengths_list)
             self.best_route = self.lengths_list.index(self.best_route_distance)
-            print(self.best_route)
+            self.best_result_list.append(self.best_route_distance)
+            self.best_route_locations = self.generation[self.best_route]
 
+    def get_roulette_wheel(self):
+        reverse_list = [(1/x)**3 for x in self.lengths_list]
+        distance_divided = [x * (100/sum(reverse_list)) for x in reverse_list]
+        roulette_wheel = [distance_divided[0]]
+        for i in range(1, len(distance_divided)-1):
+            roulette_wheel.append(distance_divided[i] + roulette_wheel[i-1])
+        self.roulette_wheel = roulette_wheel
 
+    def draw_parent(self):
+        """
+        SELECTION OPERATOR
+        :return:
+        """
+        number = random.uniform(0, 100)
+        index = 0
+        for element in self.roulette_wheel:
+            if number < element:
+                break
+            index = index + 1
+        return index
 
+    def crossover(self):
+        """
+        CROSSOVER OPERATOR
+        :return:
+        """
+        border = random.randint(1, len(self.generation[0])-3)
+        route1 = self.generation[self.draw_parent()]
+        route2 = self.generation[self.draw_parent()]
+        new_route1 = [0]
+        new_route2 = [0]
+        for i in range(1, len(route1[:-1])):
+            if i <= border:
+                if route1[i] not in new_route1:
+                    new_route1.append(route1[i])
+                if route2[i] not in new_route2:
+                    new_route2.append(route2[i])
+            else:
+                if route2[i] not in new_route1:
+                    new_route1.append(route2[i])
+                if route1[i] not in new_route2:
+                    new_route2.append(route1[i])
+        for element in route1[1:-1]:
+            if element not in new_route1:
+                new_route1.append(element)
+            if element not in new_route2:
+                new_route2.append(element)
+        new_route1.append(0)
+        new_route2.append(0)
+        return new_route1, new_route2
+
+    def update_population(self):
+        new_generation = []
+        for _ in range(int(len(self.generation)/2)):
+            children = self.crossover()
+            new_generation = list(chain(new_generation, [children[0]], [children[1]]))
+        self.generation = new_generation
+        self.number_of_generation += 1
+
+    def mutation(self, mutation_rate):
+        for route in self.generation:
+            if random.random() <= mutation_rate:
+                city1 = random.randint(1, len(route)-2)
+                city2 = random.randint(1, len(route)-2)
+                route[city1], route[city2] = route[city2], route[city1]
+
+    def get_route(self, locations):
+        route = []
+        for element in self.best_route_locations:
+            route.append(locations[element])
+        return route
