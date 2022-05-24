05a5975e17c328a1630a5df292a66d7ef0619157
janscheuring <jans@meritus.pl>
2022-05-24 16:30:10 +0200

    variable folder

---
+++ config.py
@@ -0,0 +1,22 @@
+#####################
+# Genetic algorithm #
+#####################
+mutation_rate = 0.1
+number_of_locations = 15
+solutions_in_generation = 1000
+
+#####################
+#   Visualization   #
+#####################
+width = 500
+height = 550
+iterations = 0
+state = True
+font = 'Times New Roman'
+font_size = 20
+white = (250, 250, 250)
+
+#####################
+#     Generator     #
+#####################
+minmaxCoordinates = [1, 500]
